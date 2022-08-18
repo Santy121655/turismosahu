a86ae669f4faca61119330beb6f70935e2501cfa
santiago hernandez lopez <santylopes62@gmail.com>
2022-08-18 12:19:56 -0500

    sahu

---
+++ indexx.php
@@ -0,0 +1,357 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <link rel="icon" href="images/icon.ico">
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>Turismos SAHU</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+      <!-- style css -->
+      <link rel="stylesheet" href="css/style.css">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="css/responsive.css">
+      <!-- fevicon -->
+      <link rel="icon" href="images/fevicon.png" type="image/gif" />
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+      <!-- Tweaks for older IEs-->
+      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+      <!-- owl stylesheets --> 
+      <link rel="stylesheet" href="css/owl.carousel.min.css">
+      <link rel="stylesheet" href="css/owl.theme.default.min.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+   </head>
+   <!-- body -->
+   <body>
+
+<div id="mySidebar" class="sidebar">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
+  <a href="index.html">Inicio</a>
+  <a href="about.html">Destinos </a>
+  <a href="gallery.html">Viajes</a>
+  <a href="testimonial.html">Testimonios</a>
+  <a href="contact.html">Contactanos</a>
+</div>
+<div id="main">
+      <!-- header section start -->
+      <div class="header_section">
+        <div class="container-fluid">
+         <div class="row">
+            <div class="col-sm-4">
+               <div class="search_icon"><img src="images/search-icon.png"></div>
+            </div>
+            <div class="col-sm-4">
+               <div class="logo"><img src="images/logo.png"></div>
+            </div>
+            <div class="col-sm-4">
+               <div class="togle_3">
+                  <div class="left_main">
+                     <div class="menu_main">
+                        <center><button type="button" class="login"><a href="index.php">Admin</a></button></center>
+                        
+                     </div>
+                  </div>
+                  <div class="middle_main">
+                     <div class="shoping_bag"><img src="images/shopping-bag.png"></div>
+                     
+                  </div>
+                  <div class="right_main">
+                     <div class="togle_main"><a class="class="openbtn onclick="openNav()"><img src="images/togle-menu-icon.png" style="max-width: 100%;"></a></div>
+                  </div>
+               </div>
+               </div>
+            </div>
+         </div>
+      <div class="banner_section layout_padding">
+      <div class="container">
+      <section >
+         <div id="main_slider" class="section carousel slide banner-main" data-ride="carousel">
+            <ol class="carousel-indicators">
+               <li data-target="#main_slider" data-slide-to="0" class="active"></li>
+               <li data-target="#main_slider" data-slide-to="1"></li>
+               <li data-target="#main_slider" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+               <div class="carousel-item active">
+                  <div class="container">
+                     <div class="row marginii">
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                           <div class="carousel-sporrt_text ">
+                              <h1 class="sport_text"><strong>#Vive<br><span style="color: #fff;">La experiencia</span></strong></h1>
+                              <p class="lorem_text">En esta esta ajencia de viajes queremos que formes parte de la gran  familia SAHU con todas las experecias que mes a mes preparamos para ti. </p>
+                              <button class="boynow_bt">Viajes</button>
+                              <button class="contact_bt">Contactanos</button>
+                           </div>
+                        </div>
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                           <div class="img-box">
+                              <figure><img src="images/img-1.png" style="max-width: 100%;"/></figure>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="carousel-item">
+                  <div class="container">
+                     <div class="container">
+                     <div class="row marginii">
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                           <div class="carousel-sporrt_text ">
+                              <h1 class="sport_text"><strong>Turismos<br><span style="color: #fff;">SAHU</span></strong></h1>
+                              <p class="lorem_text">En esta esta ajencia de viajes queremos que formes parte de la gran  familia SAHU con todas las experecias que mes a mes preparamos para ti. </p>
+                              <button class="boynow_bt">Viajes</button>
+                              <button class="contact_bt">Contactanos</button>
+                           </div>
+                        </div>
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                           <div class="img-box">
+                              <figure><img src="images/img-1.png" style="max-width: 100%;"/></figure>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  </div>
+               </div>
+               <div class="carousel-item">
+                  <div class="container">
+                     <div class="row marginii">
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                           <div class="carousel-sporrt_text ">
+                              <h1 class="sport_text"><strong>#Vive<br><span style="color: #fff;">La expereincia</span></strong></h1>
+                              <p class="lorem_text"> En esta esta ajencia de viajes queremos que formes parte de la gran  familia SAHU con todas las experecias que mes a mes preparamos para ti.</p>
+                              <button class="boynow_bt">Viajes</button>
+                              <button class="contact_bt">Contactanos</button>
+                           </div>
+                        </div>
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                           <div class="img-box">
+                              <figure><img src="images/img-1.png" style="max-width: 100%;"/></figure>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      </div>
+      </div>
+   </div>
+   </div>
+   <!-- about section start -->
+   <div class="container-fluid">
+   <div class="about_section">
+      <div class="container">
+         <div class="about_img"><img src="images/about-img1.png" style="max-width: 100%;"></div>
+         <h2 class="about_text"><strong>Nuevas experencias <span style="color: #fff;">SAHU</span></strong></h2>
+         <div class="about_middle">
+            <p class="about_lorem"></p>
+         </div>
+         <div class="red_bt">
+            <center><button type="button" class="main_bt"><a href="vistas/product_list.php">#SAHU</a></button></center>
+         </div>
+      </div>
+   </div>
+   </div>
+   <!-- about section end -->
+   <!-- our section start -->
+   <div class="our_section">
+      <div class="container">
+         <div class="row">
+            <div class="col-sm-12">
+               <h1 class="our_text"><strong>Proximos <span style="color: #000;">destinos</span></strong></h1>
+               <p class="about_lorem">Dale un vistazo a nuestras proximas fechas y destinos. </p>
+            </div>
+         </div>
+      </div>
+      <center class="our_section_2">
+         <div class="our_main">
+            <div id="my_slider" class="carousel slide" data-ride="carousel">
+               <div class="carousel-inner">
+               <div class="carousel-item active">
+                  <img class="d-block w-100" src="images/viajes/1.png" alt="First slide">
+               </div>
+               <div class="carousel-item">
+                  <img class="d-block w-100" src="images/viajes/2.png" alt="Second slide">
+               </div>
+               <div class="carousel-item">
+                 <img class="d-block w-100" src="images/viajes/3.png" alt="Third slide">
+               </div>
+               <div class="carousel-item">
+                  <img class="d-block w-100" src="images/viajes/4.png" alt="fourd slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="images/viajes/5.png" alt="five slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="images/viajes/6.png" alt="six slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="images/viajes/7.png" alt="seven slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="images/viajes/8.png" alt="eight slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="images/viajes/9.png" alt="nine slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="images/viajes/10.png" alt="teen slide">
+                </div>
+            </div>
+               <a class="carousel-control-prev" href="#my_slider" role="button" data-slide="prev">
+                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                 <span class="sr-only">Informacion</span>
+               </a>
+               <a class="carousel-control-next" href="#my_slider" role="button" data-slide="next">
+                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                 <span class="sr-only">Next</span>
+               </a>
+            </div>
+          </div>
+            <center><button type="button" class="main_bt"><a href="https://l.facebook.com/l.php?u=https%3A%2F%2Fapi.whatsapp.com%2Fsend%3Fphone%3D522411260075%26app%3Dfacebook%26entry_point%3Dpage_cta%26fbclid%3DIwAR13zU7wHTMsenf6yjIT6QkPw_oxchoyHugFUoTpnCsbo1t4DlYWQ6qqQSc&h=AT2VmZjs9FaxyyT3Fjc8EGcRwwK3O9YagsHF_NmmRNUqkitTLFJIw5ao_nHmrwcoG6_bZs7KR1qjMzHodjyf5yUEIJmTvT3e-P10YvEqQ_LOWoermpHc8WBR0VJzxM5Fsbh_KI6-99_VqSetzexBMA">Saber mas</a></button></center>
+      </div>
+   </div>
+   <div class="contact_section layout_padding">
+      <div class="container">
+         <h1 class="contact_text"><strong>Contactanos</strong></h1>
+      </div>
+            <div class="contact_main">
+               <div class="enput_section">
+                  <div class="email_box">
+                    <div class="input_main">
+                       <div class="container">
+                          <form action="/action_page.php">
+                            <div class="form-group">
+                              <input type="text" class="email-bt" placeholder="Nombre" name="Nombre">
+                            </div>
+                            <div class="form-group">
+                              <input type="text" class="email-bt" placeholder="Email" name="email_box">
+                            </div>
+                            <div class="form-group">
+                              <input type="text" class="email-bt" placeholder="Numero de celuar" name="Email">
+                            </div>
+                            
+                            <div class="form-group">
+                                <textarea class="massage-bt" placeholder="Mensaje" rows="5" id="comment" name="Massage"></textarea>
+                            </div>
+                          </form>   
+                       </div> 
+                       <div class="send_btn">
+                        <button type="button" class="main_bt"><a href="">Enviar</a></button>
+                       </div>                   
+                    </div>
+                 </div>
+               </div>
+            </div>
+   </div>
+   <!-- our section end -->
+   <!-- Testimonial section start -->
+   <div class="testimonial_section layout_padding">
+      <div class="container">
+         <h1 class="contact_text"><strong>Testimonios</strong></h1>
+         <p class="dolore_text">Los que forman parte de la gran famila SAHU te pueden asegurar que pasaron las mejores vacasiones de su vida.</p>
+         <div class="images_3"><img src="images/img-3.png"></div>
+         <h2 class="rooty_text">Testimonio</h2>
+         <p class="ice_text">(Noe)</p>
+         <div><img src="images/left-quote.png"></div>
+         <p class="dolore_text">Yo te puedo asegurar que mi familia se la paso genial, el servicio las unidades todo perfecto podria recomendar ampliamente al gran equipo de turismos SAHU </p>
+         <div class="right_quote"><img src="images/right-quote.png"></div>
+      </div> 
+   </div>
+
+   <!-- Testimonial section end -->
+   <!-- footer section start -->
+   <div class="footer_section layout_padding">
+      <div class="container">
+         <div class="row">
+            <div class="col-sm-4">
+               <h2 class="important_text">Te podria interesar</h2>
+               <div class="footer_menu">
+                  <ul>
+                     <li><a href="#">Inicio</a></li>
+                     <li><a href="#">Proximos destinos</a></li>
+                     <li><a href="#">Viajes</a></li>
+                     <li><a href="#">Testimonios</a></li>
+                     <li><a href="#">Contactanos</a></li>
+                  </ul>
+               </div>
+            </div>
+            <div class="col-sm-4">
+               <h2 class="important_text">Redes sociales</h2>
+               <div class="footer_menu">
+                  <ul>
+                     <li><a href="https://www.facebook.com/SAHUTOURS">Facebook</a></li>
+                     <li><a href="https://www.instagram.com/turismossahu/">Instagram</a></li>
+                     <li><a href="https://www.tiktok.com/@turismos_sahu">Tik tok</a></li>
+                     <li><a href="turismossahu@gmail.com>;">Correo electronico</a></li>
+                     <li><a href="https://turismossahu.catalog.to/?fbclid=IwAR2TRBbex0FJeJfO-H5tujZBYPfjVQjBPBIaCGoJFCx9GC334J7jf5EFaVk">Catalogo</a></li>
+                  </ul>
+               </div>
+            </div>
+            <div class="col-sm-4">
+               <h2 class="important_text">Siguenos en Tik tok</h2>
+               <p class="footer_lorem_text">Enterate de las nuevas experencias que se vive en turismos SAHU</p>
+                   <div>
+                     <center><button type="button" class="main_bt"><a href=" https://www.tiktok.com/@turismos_sahu">Siguenos</a></button></center>
+                   </div>
+                  </div>
+               </div>
+            </div>
+         </div>   
+      </div>
+   </div>
+   <!-- footer section start -->
+   <!-- copyright section start -->
+   <div class="copyright_text">Copyright 2022 totol.<a href="https://www.facebook.com/profile.php?id=100005764425058">development.com </a></div>
+
+   </div>
+
+      <!-- Javascript files-->
+      <script src="js/jquery.min.js"></script>
+      <script src="js/popper.min.js"></script>
+      <script src="js/bootstrap.bundle.min.js"></script>
+      <script src="js/jquery-3.0.0.min.js"></script>
+      <script src="js/plugin.js"></script>
+      <!-- sidebar -->
+      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+      <script src="js/custom.js"></script>
+      <!-- javascript --> 
+      <script src="js/owl.carousel.js"></script>
+      <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+      <script>
+         $(document).ready(function(){
+         $(".fancybox").fancybox({
+         openEffect: "none",
+         closeEffect: "none"
+         });
+       $('#myCarousel').carousel({
+            interval: false
+        });
+      </script>
+
+      <script>
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+</script>
+
+   </body>
+   </html>
